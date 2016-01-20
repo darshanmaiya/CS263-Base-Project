04c3f75a8a47e411ac1a4d1f3a97096b4ca4f651
Darshan <darshanmaiya@outlook.com>
2016-01-20 12:42:34 -0800

    Task Queues and Memcache Step 7

--- base/src/main/java/cs263w16/Worker.java
+++ base/src/main/java/cs263w16/Worker.java
@@ -6,9 +6,13 @@ import javax.servlet.http.*;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.memcache.ErrorHandlers;
+import com.google.appengine.api.memcache.MemcacheService;
+import com.google.appengine.api.memcache.MemcacheServiceFactory;
 
 import java.io.*;
 import java.util.Calendar;
+import java.util.logging.Level;
 
 @SuppressWarnings("serial")
 public class Worker extends HttpServlet {
@@ -17,12 +21,17 @@ public class Worker extends HttpServlet {
         String key = request.getParameter("keyname");
         String value = request.getParameter("value");
         
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        
         Entity newTask = new Entity("TaskData", key);
         newTask.setProperty("value", value);
         newTask.setProperty("date", Calendar.getInstance().getTime());
-         
+
+        // Store in datastore
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
         datastore.put(newTask);
+        
+        // Store in cache
+        MemcacheService syncCache = MemcacheServiceFactory.getMemcacheService();
+        syncCache.setErrorHandler(ErrorHandlers.getConsistentLogAndContinue(Level.INFO));
+        syncCache.put(key, newTask);
     }
 }
